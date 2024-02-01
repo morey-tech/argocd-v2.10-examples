04a0be0bb84c7f5c4e4eae8adc746990c11c4795
Christian Hernandez <christianh814@users.noreply.github.com>
2024-01-31 17:28:06 -0800

    Update post-create.sh (#1)
    Bash completion fun

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,6 +5,8 @@ echo "post-create start" >> ~/status
 # this runs in background after UI is available
 
 kind create cluster --config kind-cluster.yaml
-echo alias k=kubectl >> /home/vscode/.bashrc
+echo 'source <(kubectl completion bash)' >> /home/vscode/.bashrc
+echo 'alias k=kubectl' >> /home/vscode/.bashrc
+echo 'complete -F __start_kubectl k' >> /home/vscode/.bashrc
 
 echo "post-create complete" >> ~/status
